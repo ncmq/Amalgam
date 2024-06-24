822103ab7ff34f075a70cbdc3ff57b1ab94763e7
ncmq <146462605+ncmq@users.noreply.github.com>
2024-06-24 16:30:53 +0200

    random pitch

--- Amalgam/src/Features/PacketManip/AntiAim/AntiAim.cpp
+++ Amalgam/src/Features/PacketManip/AntiAim/AntiAim.cpp
@@ -147,12 +147,30 @@ float CAntiAim::CalculateCustomRealPitch(float flWishPitch, bool bFakeDown)
 
 float CAntiAim::GetPitch(float flCurPitch)
 {
-	const int iFake = Vars::AntiHack::AntiAim::PitchFake.Value, iReal = Vars::AntiHack::AntiAim::PitchReal.Value;
+	const int iFake = Vars::AntiHack::AntiAim::PitchFake.Value;
+	const int iReal = Vars::AntiHack::AntiAim::PitchReal.Value;
+
+	// Initialize random seed if needed
+	static bool bInitialized = false;
+	if (!bInitialized)
+	{
+		srand(static_cast<unsigned int>(time(nullptr)));
+		bInitialized = true;
+	}
+
 	switch (iReal)
 	{
-		case 1: return iFake ? CalculateCustomRealPitch(-89.f, iFake - 1) : -89.f;
-		case 2: return iFake ? CalculateCustomRealPitch(89.f, iFake - 1) : 89.f;
-		case 3: return iFake ? CalculateCustomRealPitch(0.f, iFake - 1) : 0.f;
+	case 1: return iFake ? CalculateCustomRealPitch(-89.f, iFake - 1) : -89.f;
+	case 2: return iFake ? CalculateCustomRealPitch(89.f, iFake - 1) : 89.f;
+	case 3: return iFake ? CalculateCustomRealPitch(0.f, iFake - 1) : 0.f;
+	case 4:
+		{
+		// Generate a random float between -89.0f and 89.0f
+			float randomPitch = -89.f + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (89.f - (-89.f))));
+			return randomPitch;
+		}
+
+
 	}
 
 	return iFake ? -89.f + (89.f * (iFake - 1)) : flCurPitch;
